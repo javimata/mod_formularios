4df8b7bfd38d94391536cc03a129f447c0bc0102
Javier JAM <javimata@gmail.com>
2018-02-08 19:23:53 -0600

    update

--- helper.php
+++ helper.php
@@ -1,18 +1,11 @@
 <?php
 /**
- * Helper class for Hello World! module
- * 
- * @package    Joomla.Tutorials
- * @subpackage Modules
- * @link http://docs.joomla.org/J3.x:Creating_a_simple_module/Developing_a_Basic_Module
- * @license        GNU/GPL, see LICENSE.php
- * mod_helloworld is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
+ * Helper class for mod_formularios module
  */
-class modFormularios
+
+ class modFormularios
 {
+
     /**
      * @param array $params An object containing the module parameters
      * @access public
@@ -24,10 +17,8 @@ class modFormularios
 
     }
 
-
     public static function getCampos( &$params )
     {
-
 
         $campos_lista = $params->get("campo_nombre");
 
@@ -128,10 +119,5 @@ class modFormularios
 
         return $categoriesTree;
     }
-
+   
 }
-
-
-
-
-
